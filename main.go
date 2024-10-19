d6c225d4553a40ea46ce9d1c038cb8b5d0d725e8
Sivabalan Prabhu <sivabalanprabhu44@gmail.com>
2024-10-19 10:24:39 +0530

    basic variables and conditions added

--- main.go
+++ main.go
@@ -6,4 +6,20 @@ import (
 
 func main(){
 	fmt.Println("Hellow, World!")
+	var age int = 22
+	var name string = "Sivabalan Prabhu"
+	var height float32= 6.01
+	fmt.Printf("My name is %s and my age is  %d\n",name,age)
+	fmt.Printf("my height is %.2f\n",height)
+
+	place := "Ramanathapuram"
+	runs := 100
+	fmt.Println("I am from", place)
+
+	if runs > 100{
+		fmt.Println("You have scored a century")
+	} else{
+		fmt.Println("You have scored less than a century")
+	}
+
 }
